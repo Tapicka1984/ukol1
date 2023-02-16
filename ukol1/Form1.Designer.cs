c468488deaa5884dc3efcba0d6d0f75daad8a247
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2023-02-16 16:55:35 +0100

    Add files via upload

---
+++ ukol1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ukol1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(196, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Klinka na mě! ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(433, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 433);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(178, 433);
+            this.listBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(618, 455);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ ukol1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ukol1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sude = 0;
+        int liche = 0;
+        int celkem = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader ctenar = new StreamReader("cislo.txt");
+            while (!ctenar.EndOfStream)
+            {
+                int cislo = int.Parse(ctenar.ReadLine());
+                listBox1.Items.Add(cislo);
+                celkem += cislo;
+                if ((cislo) % 2 == 0)
+                {
+                    sude++;
+                }
+                else
+                {
+                    liche++;
+                }
+            }
+            ctenar.Close();
+            StreamWriter zapis = new StreamWriter("cislo.txt", true);
+            zapis.WriteLine(celkem);
+            zapis.WriteLine(sude);
+            zapis.WriteLine(liche);
+            zapis.Close();
+            StreamReader protoze_listbox = new StreamReader("cislo.txt");
+            while (!protoze_listbox.EndOfStream)
+            {
+                listBox2.Items.Add(protoze_listbox.ReadLine());
+            }
+            protoze_listbox.Close();
+        }
+    }
+}
